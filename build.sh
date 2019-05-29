ad1d7a5eb4338d42193619c9d7a05c418238e9dc
yangzhibin <yangzhibin@peersafe.cn>
2019-05-29 12:03:25 +0800

    fix - modefy build script

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 set -x
 
 rm wide.tar wide.tar.gz
-docker rmi -f peersafe/wide:latest
+docker rmi -f peersafes/wide:latest
 GOOS=linux GOARCH=amd64 go build  -tags "nopkcs11"
 docker build --no-cache -t peersafe/wide:latest .
-#docker save peersafe/wide:latest -o wide.tar
-#tar -zcvf wide.tar.gz wide.tar
+docker save peersafes/wide:latest -o wide.tar
+tar -zcvf wide.tar.gz wide.tar workspaces conf docker-compose.yaml 
 #ssh peersafe@192.168.0.15 "cd ~/wide; rm wide.tar; docker-compose down"
 #scp wide.tar peersafe@192.168.0.15:~/wide
 #ssh peersafe@192.168.0.15 "cd ~/wide; docker load -i wide.tar; docker-compose up -d"
