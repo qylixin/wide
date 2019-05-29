2cbd6f49b86e54c44a75b1edcc01a7e5268e85c5
yangzhibin <yangzhibin@peersafe.cn>
2019-05-29 12:04:44 +0800

    fix - remove useless save

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ rm wide.tar wide.tar.gz
 docker rmi -f peersafes/wide:latest
 GOOS=linux GOARCH=amd64 go build  -tags "nopkcs11"
 docker build --no-cache -t peersafes/wide:latest .
-docker save peersafes/wide:latest -o wide.tar
-tar -zcvf wide.tar.gz wide.tar workspaces conf docker-compose.yaml 
+#docker save peersafes/wide:latest -o wide.tar
+#tar -zcvf wide.tar.gz wide.tar workspaces conf docker-compose.yaml 
 #ssh peersafe@192.168.0.15 "cd ~/wide; rm wide.tar; docker-compose down"
 #scp wide.tar peersafe@192.168.0.15:~/wide
 #ssh peersafe@192.168.0.15 "cd ~/wide; docker load -i wide.tar; docker-compose up -d"
